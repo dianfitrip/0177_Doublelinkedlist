978493cdddbe392a810fbfb607b44670f3f2eb89
dianfitrip <dianfitri683@gmail.com>
2024-05-05 02:02:42 +0700

    yg ditandai current yg di hapus

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -120,4 +120,8 @@ void deleteNode() {
 		}
 	}
 
-	
+	// release the memory of the node marked as current
+	delete current;
+	cout << "\x1b[32mRecord with roll number " << rollNo << " deleted\x1b[0m" << endl;
+}
+
