23308c596979677678ea48bbf2ed1a5e0dcbd90a
dianfitrip <dianfitri683@gmail.com>
2024-05-05 01:53:33 +0700

    looping untuk current trs berjalan selagi bkn null atau sama

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -89,4 +89,16 @@ void deleteNode() {
 	previous = NULL;
 
 	// Locate the node to be deleted
+	while (current != NULL && current->noMhs != rollNo) {
+
+		previous = current;
+		current = current->next;
+	}
+
+	if (current == NULL) {
+
+		cout << "\033[31mThe record with roll number " << rollNo << " not found\033[0m" << endl;
+		return;
+	}
+
 	
