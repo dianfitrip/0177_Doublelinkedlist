05d03a330def5b1356bce512b33e5b7f9da31c3a
dianfitrip <dianfitri683@gmail.com>
2024-05-05 02:13:21 +0700

    membaca dan menelusuri data dari akhir ke awal atau dari urutan terbaliknya

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -145,3 +145,23 @@ void traverse() {
 	}
 }
 
+void revtraverse() {
+
+	if (listEmpty())
+		cout << "\nlist is empty" << endl;
+	else {
+
+		cout << "\nRecords in descending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+
+		while (currentNode != NULL) {
+
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
+
+	}
+}
+
