19f949227f5926b6fe4ab6cd7c89646ea107a1a7
dianfitrip <dianfitri683@gmail.com>
2024-05-05 01:47:32 +0700

    proses masukin node part 2

--- DoubleLinkedList/DoubleLinkedList.cpp
+++ DoubleLinkedList/DoubleLinkedList.cpp
@@ -35,4 +35,30 @@ void addNode() {
 		START = newNode;										 // step 6: make the new node the first node
 	}
 
-	
+	else {
+		// insert the new node in the middle or at the end
+		Node* current = START;									// step 1.a: start from the first node
+		Node* previous = NULL;									// step 1.b: previous node is NULL initially
+
+
+		while (current != NULL && current->noMhs < newNode->noMhs) { // step1.c: traver
+			previous = current;									// step 1.d: move the previous to the current node
+			current = current->next;							// step 1.e: move the current to the next node
+		}
+
+		newNode->next = current;								// step 4: make the next field of the new node point t
+		newNode->prev = previous;								// step 5: make the previous field of the new node point
+
+		if (current != NULL) {
+			current->prev = newNode;							// step 6: make the previous field of the current 
+		}
+		if (previous != NULL) {
+			previous->next = newNode;							// step 7: make the next field of the previous node
+		}
+		else {
+			// if previous is still NULL, it means newNode is now the first node
+			START = newNode;
+		}
+	}
+}
+
